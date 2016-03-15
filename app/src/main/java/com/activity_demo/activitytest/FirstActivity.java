b5d2ec3bfefc5e8f393dedfb12c04f5bcb75ee20
WXB506 <1922686887@qq.com>
2016-03-15 22:13:48 +0800

    add some comments

--- app/src/main/java/com/activity_demo/activitytest/FirstActivity.java
+++ app/src/main/java/com/activity_demo/activitytest/FirstActivity.java
@@ -22,7 +22,10 @@ public class FirstActivity extends ActionBarActivity{
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // Intent 是 android 程序中各组件之间进行交互的一种重要方式，可以指明当前组件想要执行的动作，
+                // 同时还可以在不同组件之间传递数据。一般可用于启动活动、启动服务、发送广播
                 Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                // Activity类中提供了一个startActivity() 方法，专门用于启动活动，接受一个Intent参数
                 startActivity(intent);
             }
         });
